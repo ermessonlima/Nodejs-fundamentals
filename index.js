4fc545625e04815111e43ccdb95f340b513bfe67
unknown <ermessonlimadossantos@gmail.com>
2022-02-01 02:26:05 -0300

    node:fundamentals

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const { send } = require('express/lib/response');
+
+
+const app = express()
+
+app.get('/courses', (request, response) => {
+    return response.json(
+        [
+            "Curso 1",
+            "Curso 2",
+            "Curso 3"
+        ]
+    )
+})
+
+app.post("/courses", (request, response) => {
+    return response.json(
+        [
+            "Curso 1",
+            "Curso 2",
+            "Curso 3",
+            "Curso 4" 
+        ]
+    )
+})
+
+app.put("/courses/:id", (request, response) => {
+    return response.json(
+        [
+            "Curso 6",
+            "Curso 2",
+            "Curso 3",
+            "Curso 4" 
+        ]
+    )
+})
+
+app.patch("/courses/:id", (request, response) => {
+    return response.json(
+        [
+            "Curso 6",
+            "Curso 7",
+            "Curso 3",
+            "Curso 4" 
+        ]
+    )
+})
+
+app.delete("/courses/:id", (request, response) => {
+    return response.json(
+        [
+            "Curso 6",
+            "Curso 7",
+            "Curso 4" 
+        ]
+    )
+})
+//localhost: 3333
+app.listen(3333)
